d74d1ecf259ac9d218dc3ca3c9203c589850e011
soo-park <sparkstacks@gmail.com>
2017-04-30 15:18:36 -0700

    added dom element util and tests

---
+++ client/js/dom-util.js
@@ -0,0 +1,28 @@
+const removeChildren = function(parentEl) {
+  while (parentEl.firstChild) {
+    parentEl.removeChild(parentEl.firstChild);
+  }
+};
+
+const createEl = function(tagName) {
+  return function(text) {
+    // create element with particular tag name
+    const el = document.createElement(tagName);
+    if (text) {
+    // put whatever given in between the opening/closing tags
+      el.textContent = text;
+    }
+    return el;
+  };
+};
+
+const createTR = createEl('TR');
+const createTH = createEl('TH');
+const createTD = createEl('TD');
+
+module.exports = {
+  createTR: createTR,
+  createTH: createTH,
+  createTD: createTD,
+  removeChildren: removeChildren
+};
