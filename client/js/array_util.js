2d6aa25ffdf49990e002e86b9c3c6e0758f8b182
soo-park <sparkstacks@gmail.com>
2017-04-30 12:39:09 -0700

    array utility added

--- client/js/array_util.js
+++
@@ -1,8 +0,0 @@
-const getRange = function(fromNum, toNum) {
-  return Array.from({ length: toNum - fromNum +1 },
-      (unused, i) => i + fromNum);
-};
-
-module.exports = {
-  getRange: getRange
-};
