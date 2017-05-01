0a830c96ca340a1fb95ad37bbff38c9c692d809c
soo-park <sparkstacks@gmail.com>
2017-04-30 20:09:32 -0700

    table view test added

---
+++ client/js/test/table-view.test.js
@@ -0,0 +1,85 @@
+// calls for JS file system
+const fs = require('fs');
+const TableModel = require('../table-model');
+const TableView = require('../table-view');
+
+describe('table-view', () => {
+
+  beforeEach(() => {
+    // load HTML skeleton from disk and parse into the DOM
+    // the sheet-container.html is dupe of index.html
+    // but Pug or React required to be DRY
+    const fixturePath = './client/js/test/fixtures/sheet-container.html';
+    // makes synchronous function to block all tasks until this is done
+    // usually just "readFile" used, but since one-time function this OK
+    const html = fs.readFileSync(fixturePath, 'utf8');
+    document.documentElement.innerHTML = html;
+  });
+
+  describe('table body', () => {
+    it('highlights the current cell when clicked', () => {
+      // set up the initial state
+      const model = new TableModel(10, 5);
+      const view = new TableView(model);
+      view.init();
+
+      // inspect the initial state
+      let trs = document.querySelectorAll('TBODY TR');
+      let td = trs[2].cells[3];
+      expect(td.className).toBe('');
+
+      // simulate user action
+      td.click();
+
+      // inspet the resulting state
+      trs = document.querySelectorAll('TBODY TR');
+      td = trs[2].cells[3];
+      expect(td.className).not.toBe('');
+    });
+
+    it('has the right size', () => {
+      // set up the initial state
+      const numCols = 6;
+      const numRows = 10;
+      const model = new TableModel(numCols, numRows);
+      const view = new TableView(model);
+      view.init();
+
+      // inspect the initial state
+      let ths = document.querySelectorAll('THEAD TH');
+      expect(ths.length).toBe(numCols);
+    });
+
+    it('fills in values from the model', () => {
+      // set up the initial state
+      const model = new TableModel(3, 3);
+      const view = new TableView(model);
+      model.setValue({col:2, row: 1}, '123');
+      view.init()
+
+      // inspect the initial state
+      const trs = document.querySelectorAll('TBODY TR');
+      expect(trs[1].cells[2].textContent).toBe('123');      
+    });
+  });
+
+  describe('table header', () => {
+    it('has valid column header labels', () => {
+      // set up the initial state
+      const numCols = 6;
+      const numRows = 10;
+      const model = new TableModel(numCols, numRows);
+      const view = new TableView(model);
+      view.init();
+
+      // inspect the inital state
+      let ths = document.querySelectorAll('THEAD TH');
+      expect(ths.length).toBe(numCols);
+
+      // inspect the resulting state
+      let labelTexts = Array.from(ths).map(el => el.textContent);
+      expect(labelTexts).toEqual(['A', 'B', 'C', 'D', 'E', 'F']);
+    });
+  });
+
+});
